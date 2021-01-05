55345086322694d9eb50e99afc8d0dd653ab7e36
Abdallah Abis <abis.abdallah@gmail.com>
2021-01-05 14:34:07 +0100

    ➖  Removed some dependencies and cleared PWA.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-
-module.exports = withPWA({
-	pwa: {
-		disable: process.env.NODE_ENV === 'development',
-		// dest: 'public', // comment out this line
-		register: true,
-		sw: '/sw.js',
-	},
-});
+module.exports = {
+	crossOrigin: 'anonymous',
+};
