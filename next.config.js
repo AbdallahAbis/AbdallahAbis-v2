ac116e08d67bb95294a11a93edff15a1611d2a9c
Abdallah Abis <abis.abdallah@gmail.com>
2021-01-05 13:42:52 +0100

    ⚡️ Improving PWA

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true',
-});
 
-module.exports = {
-	crossOrigin: 'anonymous',
-	pwa: withPWA({
-		pwa: {
-			dest: 'public',
-			runtimeCaching,
-		},
-	}),
-	analyzer: withBundleAnalyzer({}),
-};
+module.exports = withPWA({
+	pwa: {
+		dest: 'public',
+		runtimeCaching,
+	},
+});
