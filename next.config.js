9659db021f1783b95e58b359282ca6026a72943d
Abdallah Abis <abis.abdallah@gmail.com>
2021-01-05 14:02:56 +0100

    ⚡️ Improving PWA.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
 	pwa: {
-		dest: 'public',
-		runtimeCaching,
+		disable: process.env.NODE_ENV === 'development',
+		// dest: 'public', // comment out this line
+		register: true,
+		sw: '/sw.js',
 	},
 });
