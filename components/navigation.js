c37c0fb60ef7bde3cf1e6c9e4beaebe061f91005
Abdallah Abis <abis.abdallah@gmail.com>
2021-01-05 13:27:10 +0100

    ♿️  Imporoved meta viewport for accessibilty.

--- components/navigation.js
+++ components/navigation.js
@@ -92,7 +92,7 @@ const Navigation = ({ data }) => {
 				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
 				<meta
 					name='viewport'
-					content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1'
+					content='width=device-width,initial-scale=1,user-scalable=yes'
 				/>
 				<meta
 					name='description'
