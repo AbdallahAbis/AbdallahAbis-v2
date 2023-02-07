a7a86b16116dec1a3e4ead9bfd685e98982cdae7
Abdallah Abis <59185227+AbdallahAbis@users.noreply.github.com>
2023-02-07 20:28:17 +0100

    Fix jumping styles (#17)
    * Fix jumping styles
    
    * test
    
    * test
    
    * test

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,50 @@
-// This custom Component is used to inject things that are the same for all pages in the head.
 import React from "react";
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+// Import styled components ServerStyleSheet
+import { ServerStyleSheet } from "styled-components";
 import { blockingSetInitialColorMode } from "../lib/handleTheme";
 
-export default function Document({ styleTags }) {
-  return (
-    <Html lang="en">
-      <Head>{styleTags}</Head>
-      <body>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: blockingSetInitialColorMode,
-          }}
-        ></script>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: blockingSetInitialColorMode,
+            }}
+          ></script>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
