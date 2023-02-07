d63c84f4d8ebf348815423a77801eb3bf9e9dfe0
AbdallahAbis <abis.abdallah@gmail.com>
2023-02-07 20:07:06 +0100

    Fix jumping styles

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,44 @@
-// This custom Component is used to inject things that are the same for all pages in the head.
 import React from "react";
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+// Import styled components ServerStyleSheet
+import { ServerStyleSheet } from "styled-components";
 import { blockingSetInitialColorMode } from "../lib/handleTheme";
 
-export default function Document({ styleTags }) {
-  return (
-    <Html lang="en">
-      <Head>{styleTags}</Head>
-      <body>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: blockingSetInitialColorMode,
-          }}
-        ></script>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage;
+
+    // Step 1: Create an instance of ServerStyleSheet
+    const sheet = new ServerStyleSheet();
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    // Step 3: Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement();
+
+    // Step 4: Pass styleTags as a prop
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: blockingSetInitialColorMode,
+            }}
+          ></script>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
