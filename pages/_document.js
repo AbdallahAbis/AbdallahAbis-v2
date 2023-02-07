5eb70442926a130ae8355e53c3e456b082901415
AbdallahAbis <abis.abdallah@gmail.com>
2023-02-07 20:24:22 +0100

    test

--- pages/_document.js
+++ pages/_document.js
@@ -6,23 +6,29 @@ import { blockingSetInitialColorMode } from "../lib/handleTheme";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
-    // Step 1: Create an instance of ServerStyleSheet
-    const sheet = new ServerStyleSheet();
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-      });
-
-    // Step 3: Extract the styles as <style> tags
-    const styleTags = sheet.getStyleElement();
-    // Step 4: Pass styleTags as a prop
-    const initialProps = await Document.getInitialProps(ctx);
-    console.log(initialProps);
-    
-    return { ...initialProps, styleTags };
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
